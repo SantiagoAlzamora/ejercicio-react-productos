a0b6ce4032dbec3ee915b036b676ef68f06e17da
Santiago Alzamora <santiagoalzamora06@gmail.com>
2023-09-18 14:11:30 -0300

    inital commit

---
+++ src/components/Producto.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import FavImg from '../img/me-gusta.png'
+import NoFavImg from '../img/corazon.png'
+
+export default function Producto({producto, agregarAFavoritos, eliminarDeFavoritos}) {
+
+  const[favorito, setFavorito] = useState(false)
+
+  function handleClick(){
+
+    if(favorito){
+      eliminarDeFavoritos(producto)
+      setFavorito(false)
+    }else{
+      agregarAFavoritos(producto)
+      setFavorito(true)
+    }
+  }
+
+  return (
+    <div className="producto">
+      <Link to={`/producto/${producto.id}`}><img src={producto.image} alt="" /></Link>
+      <div className="info">
+        <p>{producto.title}</p>
+        <mark>${producto.price}</mark>
+      </div>
+      {
+        favorito ? 
+        <img className='fav-icon' onClick={handleClick} src={NoFavImg} alt="" />
+        :
+        <img className='fav-icon' onClick={handleClick} src={FavImg} alt="" />
+      }
+      
+    </div>
+  )
+}
